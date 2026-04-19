4d34ce1fe51d827823bffaee613a129ef863c193
philly-7 <fesrusphilly@gmail.com>
2026-04-19 12:25:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+
 global.ownerName = "KING VOID DEV";
 global.botName = "King Void_MDX 👑";
-global.ownerNumber = "2349154472946"; 
-global.version = "V1";
+global.ownerNumber = ["2349154472946"]; // Your number
+global.prefix = ".";
+
+// Add other settings here as needed
